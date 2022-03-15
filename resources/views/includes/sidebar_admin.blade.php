3f94c0df94474eb4bb6f1cbd0354699d7aeacdeb
godgodwinter <kakadz.pro@gmail.com>
2022-03-15 10:03:35 +0700

    update some menu sidebar

--- resources/views/includes/sidebar_admin.blade.php
+++ resources/views/includes/sidebar_admin.blade.php
@@ -83,7 +83,7 @@
         </li>
 
 
-    <li class="menu-item {{$pages=='konfirmasi'?'active': ''}}">
+    {{-- <li class="menu-item {{$pages=='konfirmasi'?'active': ''}}">
         <a
             href="{{route('admin.konfirmasi')}}"
             class="menu-link"
@@ -91,7 +91,7 @@
         <i class="menu-icon fa-solid fa-clipboard-check"></i>
             <div data-i18n="Support">Konfirmasi Pembayaran</div>
         </a>
-        </li>
+        </li> --}}
 
     {{-- <li class="menu-item {{$pages=='invoice'?'active': ''}}">
         <a
@@ -102,84 +102,3 @@
             <div data-i18n="Support">Invoice</div>
         </a>
         </li> --}}
-
-<!-- Transaksi -->
-<li class="menu-header small text-uppercase"><span class="menu-header-text">Transaksi</span></li>
-<li class="menu-item {{$pages=='kategori'?'active': ''}}">
-    <a
-        href="{{route('dev.crud')}}"
-        class="menu-link"
-    >
-    <i class="menu-icon fa-solid fa-table-cells"></i>
-        <div data-i18n="Support">Kategori</div>
-    </a>
-    </li>
-
-<!-- Layouts -->
-<li class="menu-item">
-<a href="javascript:void(0);" class="menu-link menu-toggle">
-    {{-- <i class="menu-icon tf-icons bx bx-steam"></i> --}}
-    <i class="menu-icon tf-icons fa-solid fa-book-journal-whills"></i>
-    <div data-i18n="Layouts">Layouts</div>
-</a>
-
-<ul class="menu-sub">
-    <li class="menu-item">
-    <a href="layouts-without-menu.html" class="menu-link">
-        <div data-i18n="Without menu">Profile</div>
-    </a>
-    </li>
-    <li class="menu-item">
-    <a href="layouts-without-navbar.html" class="menu-link">
-        <div data-i18n="Without navbar">Form Create</div>
-    </a>
-    </li>
-    <li class="menu-item">
-    <a href="layouts-container.html" class="menu-link">
-        <div data-i18n="Container">Form Edit</div>
-    </a>
-    </li>
-    <li class="menu-item">
-    <a href="layouts-fluid.html" class="menu-link">
-        <div data-i18n="Fluid">Settings</div>
-    </a>
-    </li>
-    <li class="menu-item">
-    <a href="layouts-blank.html" class="menu-link">
-        <div data-i18n="Blank">Login</div>
-    </a>
-    </li>
-</ul>
-</li>
-
-<!-- Forms & Tables -->
-<li class="menu-header small text-uppercase"><span class="menu-header-text">Forms &amp; Tables</span></li>
-<!-- Tables -->
-<li class="menu-item">
-<a href="tables-basic.html" class="menu-link">
-    <i class="menu-icon tf-icons fa-solid fa-table"></i>
-    <div data-i18n="Tables">Tables</div>
-</a>
-</li>
-<!-- Misc -->
-<li class="menu-header small text-uppercase"><span class="menu-header-text">Misc</span></li>
-<li class="menu-item">
-<a
-    href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
-    target="_blank"
-    class="menu-link"
->
-<i class="menu-icon tf-icons fa-solid fa-phone"></i>
-    <div data-i18n="Support">Support</div>
-</a>
-</li>
-<li class="menu-item">
-<a
-    href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/"
-    target="_blank"
-    class="menu-link"
->
-<i class="menu-icon tf-icons fa-solid fa-passport"></i>
-    <div data-i18n="Documentation">Documentation</div>
-</a>
-</li>
